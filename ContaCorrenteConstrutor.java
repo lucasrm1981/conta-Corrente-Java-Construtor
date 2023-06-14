defcd32384c1b8a82f4d32d6ff177c1e38769b87
Lucas Rodrigues Monteiro <47841382+lucasrm1981@users.noreply.github.com>
2023-06-14 16:45:44 -0300

    Update ContaCorrenteConstrutor.java

--- ContaCorrenteConstrutor.java
+++ ContaCorrenteConstrutor.java
@@ -1,10 +1,5 @@
 package contacorrenteconstrutor;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 import java.util.Scanner;
 
 /**
